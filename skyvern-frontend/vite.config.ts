163eeab52d9c197147e97c80a1559fbecd697c9d
TTWH <190041341+thinkthenwaithi@users.noreply.github.com>
2025-02-14 16:40:13 -0800

    Update vite.config.ts

--- skyvern-frontend/vite.config.ts
+++ skyvern-frontend/vite.config.ts
@@ -6,10 +6,10 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
+    port: 9001,
   },
   preview: {
-    port: 8080,
+    port: 9001,
   },
   resolve: {
     alias: {
